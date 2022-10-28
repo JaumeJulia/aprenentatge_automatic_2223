f8889d1b504281ceebdc519af3b2cdbb0cf6c4b2
JaumeJulia <Jaumejuli588@gmail.com>
2022-10-28 20:58:06 +0200

    Exercici 5 fet

--- 05_Generative_Models/main.py
+++ 05_Generative_Models/main.py
@@ -19,3 +19,63 @@ plot_digits(digits.data[:100, :])
 
 plt.show()
 print(digits.data.shape)
+
+#Aplicam el PCA
+
+pca = PCA()
+pca.fit(digits.data)
+
+varCum = np.cumsum(pca.explained_variance_ratio_)
+plt.plot(varCum)
+plt.show()
+
+#aplanar la curva con una funcion
+
+i = 0
+for iter in varCum:
+    i += 1
+    if iter >= 0.95:
+        n_components = varCum[:i]
+        break
+
+#Tornam a aplicar el PCA, ara amb les components necessaries
+
+pca = PCA(n_components = len(n_components))
+pca.fit(digits.data)
+pcaDigits = pca.transform(digits.data)
+
+varCum = np.cumsum(pca.explained_variance_ratio_)
+plt.plot(varCum)
+plt.show()
+
+#Usam ara el GaussianMixture model
+
+bicData = []
+minimo = 0
+valorMinimo = 999999
+for i in range(1, 11):
+    gm = GaussianMixture(n_components=i)
+    gm.fit(pcaDigits)
+    bic = gm.bic(pcaDigits)
+    bicData.append(bic)
+    if valorMinimo > bic:
+        valorMinimo = bic
+        minimo = i
+
+x = np.array(bicData)
+plt.plot(x)
+plt.show()
+
+## Observant el gràfic, el valor mínim es 7 que es el valor de gaussianes que millor s'adapten
+
+#Generam noves mostres
+
+gm = GaussianMixture(n_components=minimo)
+gm.fit(pcaDigits)
+x, y = gm.sample(100)
+
+#Desfeim la transformació del PCA i mostram els resultats
+
+x = pca.inverse_transform(x)
+plt.scatter(x[:, 0], x[:, 1], c=y)
+plt.show()
