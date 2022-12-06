5050d670e6fb8ceab9123d52b66ff752e20cde99
pablo-tic <pablo200055@gmail.com>
2022-12-06 17:30:32 +0100

    Funciones de Características
    He quitado el contarLletres, ya que con un "len" bastaba.

--- Practica1/main.py
+++ Practica1/main.py
@@ -19,4 +19,73 @@ angles=reformat(angles)
 wordsDF=pd.concat([catala,angles], axis=0)
 #Shuffle the rows
 wordsDF = wordsDF.sample(frac=1).reset_index(drop=True)
+
+
+#ratio de consonantes y vocales
+def ratio (word):
+    vocals=0
+    for c in word:
+        if isVocal(c):
+            vocals+=1
+    return vocals/len(word) 
+
+#isVocal?
+def isVocal(c):
+    if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u'):
+        return True
+    return 
+#gotAccent?
+def gotAccent(word):
+    #List containing all possible accentuated chars from Catalan
+    accentuatedChars=[ord('à'),ord('è'),ord('é'),ord('í'),ord('ò'),ord('ó'),ord('ú')]
+    for c in word:
+        if ord(c) in accentuatedChars:
+            return 1
+    return 0
+def doubleVocal(word):
+    ocurrences=["aa","ee","ii","oo","uu"]
+    for oc in ocurrences:
+        if word.find(oc)!=-1:
+            return 1
+    return 0
+
+def enCC(word):
+    ocurrences=["sch","spl","shr","squ","thr","spr","scr","sph","th","tw","sw","sk","sm"]
+    for oc in ocurrences:
+        if word.find(oc)!=-1:
+            return 1
+    return 0
+'''
+def doubleVocal(word):
+    possibleDuo=False
+    vocal = ' '
+    for c in word:
+        if possibleDuo==True:
+            #Possible Duo Found?
+            if isVocal(c):
+                if c == vocal:
+                    #Duo Found
+                    return 1
+                else:
+                    #New Possible Duo
+                    vocal = c
+            #Not Found 
+            else:
+                possibleDuo=False
+        else:
+            if isVocal(c):
+                #Possible Duo in the future
+                vocal=c
+                possibleDuo=True
+    return 0
+'''
+
+
+wordsDF['ratio']=wordsDF['word'].apply(ratio)
+wordsDF['cantidadLetras']=wordsDF['word'].apply(len)
+wordsDF['gotAccent']=wordsDF['word'].apply(gotAccent)
+wordsDF['doubleVocal']=wordsDF['word'].apply(doubleVocal)
+wordsDF['enCC']=wordsDF['word'].apply(enCC)
+wordsDF=wordsDF[['word','ratio','cantidadLetras','gotAccent','doubleVocal','enCC','y']]
 wordsDF.to_csv('data/definitiveData.csv', index=False)
+print(wordsDF)
